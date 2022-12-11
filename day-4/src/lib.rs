8a4dcaf052f8c044eb78df6bedc7760b41cdf212
Gregory Edison <gregory.edison1993@gmail.com>
2022-12-11 12:28:37 +0100

    feat: clean + day-4 part 2

--- day-4/src/lib.rs
+++ day-4/src/lib.rs
@@ -3,23 +3,36 @@ use std::fs::read_to_string;
 
 fn read_file(path: &str) -> Result<Vec<String>, Error> {
     return Ok(read_to_string(path)?
-        .split("\n")
+        .split(|c| c == '\n' || c == '-' || c == ',')
         .map(|x| x.to_string())
         .collect());
 }
 
-fn count_ranges(mut input: Vec<String>) -> u32 {
+fn count_ranges_1(mut input: Vec<String>) -> u32 {
     input
-        .into_iter()
+        .chunks(4)
         .map(|x| {
-            let y: Vec<&str> = x.split(",").collect();
-            let v_1: Vec<String> = y[0].split("-").map(|c| c.to_string()).collect();
-            let v_2: Vec<String> = y[1].split("-").map(|c| c.to_string()).collect();
-            let num_1 = v_1[0].parse::<u32>().unwrap();
-            let num_2 = v_1[1].parse::<u32>().unwrap();
-            let num_3 = v_2[0].parse::<u32>().unwrap();
-            let num_4 = v_2[1].parse::<u32>().unwrap();
+            let num_1 = x[0].parse::<u32>().unwrap();
+            let num_2 = x[1].parse::<u32>().unwrap();
+            let num_3 = x[2].parse::<u32>().unwrap();
+            let num_4 = x[3].parse::<u32>().unwrap();
             if num_1 <= num_3 && num_2 >= num_4 || num_3 <= num_1 && num_2 <= num_4 {
+                return 1;
+            }
+            return 0;
+        })
+        .sum()
+}
+
+fn count_ranges_2(input: Vec<String>) -> u32 {
+    input
+        .chunks(4)
+        .map(|x| {
+            let num_1 = x[0].parse::<u32>().unwrap();
+            let num_2 = x[1].parse::<u32>().unwrap();
+            let num_3 = x[2].parse::<u32>().unwrap();
+            let num_4 = x[3].parse::<u32>().unwrap();
+            if num_2 >= num_3 && num_1 <= num_4 {
                 return 1;
             }
             return 0;
@@ -35,14 +48,23 @@ mod tests {
     fn test_read_file() {
         let f = read_file("./src/data/test.txt");
         assert_eq!(
-            vec!["2-4,6-8", "2-3,4-5", "5-7,7-9", "2-8,3-7", "6-6,4-6", "2-6,4-8"],
+            vec![
+                "2", "4", "6", "8", "2", "3", "4", "5", "5", "7", "7", "9", "2", "8", "3", "7",
+                "6", "6", "4", "6", "2", "6", "4", "8"
+            ],
             f.unwrap()
         );
     }
 
     #[test]
-    fn test_count_range() {
-        let r = count_ranges(read_file("./src/data/data.txt").unwrap());
+    fn test_count_range_1() {
+        let r = count_ranges_1(read_file("./src/data/data.txt").unwrap());
         assert_eq!(511, r);
+    }
+
+    #[test]
+    fn test_count_range_2() {
+        let r = count_ranges_2(read_file("./src/data/data.txt").unwrap());
+        assert_eq!(821, r);
     }
 }
